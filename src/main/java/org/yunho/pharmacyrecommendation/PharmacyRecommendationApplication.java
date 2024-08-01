fbbd76ad769620bb35487c42eb7a3e648fe563d5
yunho <nam100400@naver.com>
2024-08-01 14:21:05 +0900

    first commit

---
+++ src/main/java/org/yunho/pharmacyrecommendation/PharmacyRecommendationApplication.java
@@ -0,0 +1,13 @@
+package org.yunho.pharmacyrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyRecommendationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yunho/pharmacyrecommendation/PharmacyRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yunho.pharmacyrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
