4cba5626664bdf0df20b785ca9965122b9d33731
YunhoNam <nam100400@naver.com>
2024-08-01 23:04:21 +0900

    #3 - kakao 주소검색 api 테스트 코드 작성

--- src/test/java/org/yunho/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.java
+++ src/test/java/org/yunho/pharmacyrecommendation/pharmacy/repository/PharmacyRepositoryTest.java
@@ -1,21 +1,30 @@
 package org.yunho.pharmacyrecommendation.pharmacy.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.yunho.pharmacyrecommendation.AbstractIntegrationContainerBaseTest;
 import org.yunho.pharmacyrecommendation.pharmacy.entity.Pharmacy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
+public class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
     private PharmacyRepository pharmacyRepository;
 
+    @BeforeEach
+    public void setup() {
+        pharmacyRepository.deleteAll();
+    }
+
     @Test
-    void pharmacyRepositorySave() {
+    public void testPharmacyRepositorySave() {
+        // given
         String address = "서울 특별시 성북구 종암동";
         String name = "은혜 약국";
         double latitude = 36.11;
@@ -28,11 +37,37 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
                 .longitude(longitude)
                 .build();
 
+        // when
         Pharmacy result = pharmacyRepository.save(pharmacy);
 
+        // then
         assertEquals(address, result.getPharmacyAddress());
         assertEquals(name, result.getPharmacyName());
         assertEquals(latitude, result.getLatitude());
         assertEquals(longitude, result.getLongitude());
+    }
+
+    @Test
+    public void testPharmacyRepositorySaveAll() {
+        // given
+        String address = "서울 특별시 성북구 종암동";
+        String name = "은혜 약국";
+        double latitude = 36.11;
+        double longitude = 128.11;
+
+        Pharmacy pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+
+        // when
+        pharmacyRepository.saveAll(Collections.singletonList(pharmacy));
+        Iterable<Pharmacy> result = pharmacyRepository.findAll();
+
+        // then
+        long count = result.spliterator().getExactSizeIfKnown(); // Count number of items
+        assertEquals(1, count);
     }
 }
