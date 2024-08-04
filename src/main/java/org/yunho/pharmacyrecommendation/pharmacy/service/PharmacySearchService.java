61961fb7e1c915a6b157d952a4a57f23188e2c60
YunhoNam <nam100400@naver.com>
2024-08-05 01:20:47 +0900

    #23 - Redis 이용하여 성능 개선

--- src/main/java/org/yunho/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
+++ src/main/java/org/yunho/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
@@ -3,6 +3,7 @@ package org.yunho.pharmacyrecommendation.pharmacy.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.yunho.pharmacyrecommendation.pharmacy.cache.PharmacyRedisTemplateService;
 import org.yunho.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
 import org.yunho.pharmacyrecommendation.pharmacy.entity.Pharmacy;
 
@@ -15,10 +16,16 @@ import java.util.stream.Collectors;
 public class PharmacySearchService {
 
     private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     public List<PharmacyDto> searchPharmacyDtoList() {
 
         // redis
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+        if(!pharmacyDtoList.isEmpty()) {
+            log.info("redis findAll success!");
+            return pharmacyDtoList;
+        }
 
         // db
         return pharmacyRepositoryService.findAll()
