62034ec1d5889f31c62259a76c82571e60d27fe8
YunhoNam <nam100400@naver.com>
2024-08-04 18:11:17 +0900

    #11 - 거리 계산 알고리즘 구현

--- src/main/java/org/yunho/pharmacyrecommendation/api/dto/DocumentDto.java
+++ src/main/java/org/yunho/pharmacyrecommendation/api/dto/DocumentDto.java
@@ -2,11 +2,13 @@ package org.yunho.pharmacyrecommendation.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentDto {
---
+++ src/main/java/org/yunho/pharmacyrecommendation/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,40 @@
+package org.yunho.pharmacyrecommendation.pharmacy.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.yunho.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
+import org.yunho.pharmacyrecommendation.pharmacy.entity.Pharmacy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+
+        // redis
+
+        // db
+        return pharmacyRepositoryService.findAll()
+                .stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+}
