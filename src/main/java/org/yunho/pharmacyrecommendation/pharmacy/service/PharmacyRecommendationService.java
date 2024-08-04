a6805b9fc0ad6d9ba8cd47a719937704c5cef716
YunhoNam <nam100400@naver.com>
2024-08-05 00:05:37 +0900

    #21 - shorten url 개발
    https://github.com/seruco/base62
    를 참고하여 구현하였음

--- src/main/java/org/yunho/pharmacyrecommendation/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/org/yunho/pharmacyrecommendation/pharmacy/service/PharmacyRecommendationService.java
@@ -2,6 +2,7 @@ package org.yunho.pharmacyrecommendation.pharmacy.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -10,6 +11,7 @@ import org.yunho.pharmacyrecommendation.api.dto.KakaoApiResponseDto;
 import org.yunho.pharmacyrecommendation.api.service.KakaoAddressSearchService;
 import org.yunho.pharmacyrecommendation.direction.dto.OutputDto;
 import org.yunho.pharmacyrecommendation.direction.entity.Direction;
+import org.yunho.pharmacyrecommendation.direction.service.Base62Service;
 import org.yunho.pharmacyrecommendation.direction.service.DirectionService;
 
 import java.util.Collections;
@@ -24,9 +26,12 @@ public class PharmacyRecommendationService {
 
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
+    private final Base62Service base62Service;
 
     private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
-    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
+    @Value("${pharmacy.recommendation.base.url}")
+    private String baseUrl;
 
     public List<OutputDto> recommendPharmacyList(String address) {
 
@@ -53,17 +58,10 @@ public class PharmacyRecommendationService {
 
     private OutputDto convertToOutputDto(Direction direction) {
 
-        String params = String.join(",", direction.getTargetPharmacyName(),
-                String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
-
-        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params)
-                .toUriString();
-        log.info("direction params: {}, url: {}" ,params ,result);
-
         return OutputDto.builder()
                 .pharmacyName(direction.getTargetPharmacyName())
                 .pharmacyAddress(direction.getTargetAddress())
-                .directionUrl(result)
+                .directionUrl(baseUrl + base62Service.encodeDirectionId(direction.getId()))
                 .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f km", direction.getDistance()))
                 .build();
